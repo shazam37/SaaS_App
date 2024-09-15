ef3b687eab4dba619c0ac73c8a797a96c21b5e3b
Shazam <shazajmal37@gmail.com>
2024-09-15 18:50:16 +0530

    Test1

---
+++ src/cfehome/views.py
@@ -0,0 +1,21 @@
+import pathlib
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from visits.models import PageVisit
+
+this_dir = pathlib.Path(__file__).resolve().parent 
+
+def index(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    my_title = "Hello World!"
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "total_visit_count": qs.count()
+    }
+    path = request.path
+    html_template = "home.html"
+    PageVisit.objects.create(path=request.path)
+    return render(request, html_template, my_context)
---
+++ src/visits/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class PageVisit(models.Model):
+    # db -> table
+    # id -> primary
+    path = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
